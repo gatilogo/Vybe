3d4f7429ef09d6baeadcb784c2abdc2d643aa9c5
Mohammad Kebbi <kebbi@ualberta.ca>
2019-10-27 14:02:09 -0600

    update documentation for Vibe Classes

--- app/src/main/java/com/example/vybe/vibe/AbstractVibe.java
+++ app/src/main/java/com/example/vybe/vibe/AbstractVibe.java
@@ -2,22 +2,47 @@ package com.example.vybe.vibe;
 
 import java.io.Serializable;
 
+/**
+ * This is a class that keeps track of a Vibe's color and it's emoticon.
+ * It implements Serializable interface
+ */
 public abstract class AbstractVibe implements Serializable {
     protected int color;
     protected int emoticon;
 
+    /**
+     * This gets the colorspace value for the vibe's color
+     * @return
+     * the vibe's color
+     * */
     public int getColor() {
         return color;
     }
 
+
+    /**
+     * This Constructor sets a vibe's color
+     * @param  color
+     * color of the vibe
+     */
     public void setColor(int color) {
         this.color = color;
     }
 
+    /**
+     * This gets the drawable value for the vibe's emoticon
+     * @return
+     * the vibe's emoticon
+     * */
     public int getEmoticon() {
         return emoticon;
     }
 
+    /**
+     * This Constructor sets a vibe's emoticon
+     * @param  emoticon
+     * emoticon of the vibe
+     */
     public void setEmoticon(int emoticon) {
         this.emoticon = emoticon;
     }
