9b5d3b93ee3d0138a308960475ac51547359fbb3
Mohammad Kebbi <kebbi@ualberta.ca>
2019-10-27 14:02:09 -0600

    update documentation for Vibe Classes

--- app/src/main/java/com/example/vybe/vibe/Surprise.java
+++ app/src/main/java/com/example/vybe/vibe/Surprise.java
@@ -2,13 +2,25 @@ package com.example.vybe.vibe;
 
 import com.example.vybe.R;
 
+/**
+ * This is a class that encapsulates the Surprise emotion's emoticon and color
+ * It extends the AbstractVibe Class and overrides toString's method
+ */
 public class Surprise extends AbstractVibe {
 
+    /**
+     * This Constructor sets the new vibe's color and emoticon
+     */
     public Surprise() {
         this.color = R.color.Orange;
         this.emoticon = R.drawable.surprise;
     }
 
+    /**
+     * This overrides the toString method to get the string value of the Sad class
+     * @return
+     * The string that represents the Surprise class
+     */
     @Override
     public String toString() {
         return "Surprise";
