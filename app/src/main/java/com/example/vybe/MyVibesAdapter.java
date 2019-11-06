9e07e24f20473c25df8adb08b7ba48a613158e31
Trishia Gatilogo <gatilogo@ualberta.ca>
2019-11-06 15:14:48 -0700

    made printing the adapter look nice

--- app/src/main/java/com/example/vybe/MyVibesAdapter.java
+++ app/src/main/java/com/example/vybe/MyVibesAdapter.java
@@ -10,7 +10,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MyVibesAdapter extends ArrayAdapter<VibeEvent> {
 
@@ -38,13 +42,20 @@ public class MyVibesAdapter extends ArrayAdapter<VibeEvent> {
 
         TextView dateField = view.findViewById(R.id.view_date_text_view);
         TextView reasonField = view.findViewById(R.id.my_reason_text_view);
-        TextView socialSituationField = view.findViewById(R.id.my_social_situation_text_view);
 
         VibeEvent vibeEvent = vibeEventList.get(position);
 
-        dateField.setText(vibeEvent.getDateTime().toString());
-        reasonField.setText(vibeEvent.getReason());
-        socialSituationField.setText(vibeEvent.getSocialSituation());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(" MM, YYYY h:mm a", Locale.ENGLISH);
+        LocalDateTime dateTime = vibeEvent.getDateTime();
+        String month = dateTime.getMonth().getDisplayName(TextStyle.FULL_STANDALONE, Locale.ENGLISH);
+
+        dateField.setText(month + dateTime.format(formatter));
+
+        if (vibeEvent.getReason() == null) {
+            reasonField.setVisibility(View.GONE);
+        } else {
+            reasonField.setText(vibeEvent.getReason());
+        }
 
         return view;
     }
