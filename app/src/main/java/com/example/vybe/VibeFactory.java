75a74d5fb12c302894fd39f779aa79478c792239
raysarinas <raysarinas97@gmail.com>
2019-11-07 11:28:00 -0700

    add new vector assets and vibefactory with tests

---
+++ app/src/main/java/com/example/vybe/VibeFactory.java
@@ -0,0 +1,37 @@
+package com.example.vybe;
+
+import com.example.vybe.vibefactory.Vibe;
+import com.example.vybe.vibefactory.AngryVibe;
+import com.example.vybe.vibefactory.DisgustVibe;
+import com.example.vybe.vibefactory.FearVibe;
+import com.example.vybe.vibefactory.HappyVibe;
+import com.example.vybe.vibefactory.SadVibe;
+import com.example.vybe.vibefactory.SurpriseVibe;
+
+public class VibeFactory {
+
+    public static Vibe getVibe(String selectedVibe) {
+        selectedVibe = selectedVibe.toLowerCase();
+
+        if (selectedVibe.equals("angry")) {
+            return new AngryVibe();
+        }
+        else if (selectedVibe.equals("disgust")) {
+            return new DisgustVibe();
+        }
+        else if (selectedVibe.equals("fear")) {
+            return new FearVibe();
+        }
+        else if (selectedVibe.equals("happy")) {
+            return new HappyVibe();
+        }
+        else if (selectedVibe.equals("sad")) {
+            return new SadVibe();
+        }
+        else if (selectedVibe.equals("surprise")) {
+            return new SurpriseVibe();
+        }
+
+        return null;
+    }
+}
