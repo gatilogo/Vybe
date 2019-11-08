f45d306056eb8c9886d71b859bdea245f8a7423a
raysarinas <raysarinas97@gmail.com>
2019-11-07 17:29:08 -0700

    started log in page

--- app/src/main/java/com/example/vybe/MainActivity.java
+++ app/src/main/java/com/example/vybe/MainActivity.java
@@ -4,15 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, MyVibesActivity.class);
-        startActivity(intent);
+        loginButton = findViewById(R.id.login_button);
+        signupButton = findViewById(R.id.signup_button);
+
+        loginButton.setOnClickListener(view -> {
+
+            Intent intent = new Intent(MainActivity.this, MyVibesActivity.class);
+            startActivity(intent);
+        });
+
+        signupButton.setOnClickListener(view -> {
+
+            Intent intent = new Intent(MainActivity.this, MyVibesActivity.class);
+            startActivity(intent);
+        });
     }
 }
