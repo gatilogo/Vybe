84f79803f91e25c27bd68dd554e932a2a7ec13a4
thebromoe <kebbi@ualberta.ca>
2019-11-08 15:02:56 -0700

    start on UI intent tests

---
+++ app/src/androidTest/java/com/example/vybe/MainActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.vybe;
+
+import android.app.Activity;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.robotium.solo.Solo;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Test class for MainActivity. All the UI tests are written here. Robotium test framework is
+ used
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest{
+
+}
