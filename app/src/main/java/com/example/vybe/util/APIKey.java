fac86a2ab20bdfbc81530140ce24bbae7e7a8be0
EPostill <postill@ualberta.ca>
2019-11-14 12:51:41 -0700

    removed api key

--- app/src/main/java/com/example/vybe/util/APIKey.java
+++
@@ -1,8 +0,0 @@
-package com.example.vybe.util;
-
-/*
-Class for the google API key
- */
-public class APIKey {
-    public static final String API_KEY = "AIzaSyAQ0WGTWMUV_WBRCfc7B-_HifGn9sXIUV4";
-}
