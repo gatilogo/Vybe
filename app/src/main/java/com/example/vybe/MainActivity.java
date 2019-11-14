07884072b010efb83efc043e725c6bdc88a79384
Jacob Wong <jdw2@ualberta.ca>
2019-11-13 21:17:43 -0700

    Implemented Sign-in functionality

--- app/src/main/java/com/example/vybe/MainActivity.java
+++ app/src/main/java/com/example/vybe/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.vybe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * This Activity displays the screen for a user to log in. This file will be updated
@@ -14,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     Button loginButton;
     Button signupButton;
+    EditText usernameField;
+    EditText emailField;
+    EditText passwordField;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +37,47 @@ public class MainActivity extends AppCompatActivity {
 
         loginButton = findViewById(R.id.login_button);
         signupButton = findViewById(R.id.signup_button);
+        usernameField = findViewById(R.id.username_edit_text);
+        emailField = findViewById(R.id.email_edit_text);
+        passwordField = findViewById(R.id.password_edit_text);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                // User is not logged in
+                if (user == null){
+                    Toast.makeText(getApplicationContext(), "Aight imma head out", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Aight imma stay in", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
 
         loginButton.setOnClickListener(view -> {
+            if (!isEmpty(usernameField) && !isEmpty(emailField) && !isEmpty(passwordField)){
+                // TODO: Put this "getter" into a function call maybe
+                String username = usernameField.getText().toString().trim();
+                String email = emailField.getText().toString().trim();
+                String password = passwordField.getText().toString().trim();
 
-            Intent intent = new Intent(MainActivity.this, MyVibesActivity.class);
-            startActivity(intent);
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(MainActivity.this, MyVibesActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login Failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+
         });
 
         signupButton.setOnClickListener(view -> {
@@ -34,5 +85,23 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, MyVibesActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private boolean isEmpty(EditText et){
+        return et.getText().toString().trim().length() == 0;
     }
 }
