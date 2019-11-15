b08a6acc3ec60a4cf9668c4abda15aa7eb241119
thebromoe <kebbi@ualberta.ca>
2019-11-15 13:46:51 -0700

    update AddEditVibeActivity Tests

--- app/src/androidTest/java/com/example/vybe/AddEditVibeActivityTest.java
+++ app/src/androidTest/java/com/example/vybe/AddEditVibeActivityTest.java
@@ -9,6 +9,7 @@ import androidx.test.rule.ActivityTestRule;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,12 +18,15 @@ import android.widget.TimePicker;
 import com.robotium.solo.Solo;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+
+import in.goodiebag.carouselpicker.CarouselPicker;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
@@ -56,21 +60,25 @@ public class AddEditVibeActivityTest {
     }
 
     @Test
-    public void VibeSpinnerTest(){
+    public void VibeCarousel_DialogOpen_True(){
+        // WIP: Blocking other issues, will continue working on this test later
         // Check That selector starts not selected on a vibe
-        boolean result = solo.isSpinnerTextSelected(0, "Select a vibe");
-        assertEquals("Select a vibe not selected", true, result);
+        ImageView image = (ImageView) solo.getView("vibe_selector");
 
-        solo.pressSpinnerItem(0, 1);
+        solo.clickOnView(image);
 
-        boolean actual = solo.isSpinnerTextSelected(0, "Angry");
+        solo.clickOnButton("OK");
 
-        assertEquals("spinner item Angry is not selected",true, actual);
+        solo.waitForDialogToClose();
 
+        ImageView verify = solo.getImage(0);
+
+        assertEquals(image, verify);
     }
 
+
     @Test
-    public void SocialSpinnerTest(){
+    public void SocialSpinner_SelectOptions_Pass(){
         // Check That selector starts not selected on a vibe
         boolean result = solo.isSpinnerTextSelected(1, "Select a Social Situation");
         assertEquals("Select a SC not selected", true, result);
@@ -84,34 +92,36 @@ public class AddEditVibeActivityTest {
     }
 
     @Test
-    public void stringReasonValidTest(){
+    public void StringReason_EnterText_Valid(){
+        EditText editText = (EditText) solo.getView("reason_edit_text");
         //Click on Empty Text View
-        solo.clickOnView(solo.getView("reason_edit_text"));
+        solo.clickOnView(editText);
         //Enter the text: 'asdfg'
-        solo.clearEditText((android.widget.EditText) solo.getView("reason_edit_text"));
-        solo.enterText((android.widget.EditText) solo.getView("reason_edit_text"), "asdfg");
+        solo.clearEditText(editText);
+        solo.enterText(editText, "asdfg");
         solo.searchEditText("asdfg");
         // Validate the text on the TextView
         assertEquals("Text should be the field value", "asdfg",
-                ((EditText) solo.getView("reason_edit_text")).getText().toString());
+                (editText.getText().toString()));
 
     }
 
     @Test
-    public void stringReasonErrorTest(){
+    public void StringReason_EnterText_Invalid(){
         // test if more than 3 words is provided
-        solo.clickOnView(solo.getView("reason_edit_text"));
+        EditText editText = (EditText) solo.getView("reason_edit_text");
+        solo.clickOnView(editText);
         //Enter the text: 'as df ga sd'
-        solo.clearEditText((android.widget.EditText) solo.getView("reason_edit_text"));
-        solo.enterText((android.widget.EditText) solo.getView("reason_edit_text"), "as df ga sd");
+        solo.clearEditText(editText);
+        solo.enterText(editText, "as df ga sd");
         solo.clickOnButton("Save");
 
         assertEquals("Expecting an error", "Max 3 words allowed",
-                ((EditText) solo.getView("reason_edit_text")).getError().toString());
+                (editText.getError().toString()));
     }
 
     @Test
-    public void datePickerTest(){
+    public void DatePicker_SelectDate_Pass(){
         solo.clickOnEditText(0);
         DatePicker datePicker = solo.getView(DatePicker.class, 0); // the second param is an index
         solo.setDatePicker(datePicker, 2019, 10, 8);
@@ -125,8 +135,6 @@ public class AddEditVibeActivityTest {
         String textInDateTimeField = ((EditText) solo.getView("date_time_edit_text")).getText().toString();
         assertEquals("Nov 08 2019, 04:20 AM", textInDateTimeField);
     }
-
-
 
     @After
     public void tearDown() throws Exception{
