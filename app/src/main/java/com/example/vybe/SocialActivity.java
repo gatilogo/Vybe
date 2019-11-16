6cd27f66754ab74adc7955e01159df6d01e91e66
Trishia Gatilogo <gatilogo@ualberta.ca>
2019-11-16 14:59:25 -0700

    directed to myvibesactivity on myvibesbtn click

--- app/src/main/java/com/example/vybe/SocialActivity.java
+++ app/src/main/java/com/example/vybe/SocialActivity.java
@@ -12,6 +12,7 @@ public class SocialActivity extends AppCompatActivity {
 
     private static final String TAG = "SocialActivity";
 
+    private Button myVibesBtn;
     private Button searchBtn;
 
     @Override
@@ -20,7 +21,15 @@ public class SocialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_social);
         Log.d(TAG, "onCreate: In social");
 
+        myVibesBtn = findViewById(R.id.my_vibes_btn);
         searchBtn = findViewById(R.id.search_btn);
+
+        myVibesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
