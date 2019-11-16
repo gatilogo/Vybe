69761dcf90f20ab7efa9ae55469853758b79e5cd
Trishia Gatilogo <gatilogo@ualberta.ca>
2019-11-16 14:42:59 -0700

    search works

---
+++ app/src/main/java/com/example/vybe/ProfileAdapter.java
@@ -0,0 +1,47 @@
+package com.example.vybe;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class ProfileAdapter extends ArrayAdapter<User> {
+
+    private static final String TAG = "ProfileAdapter";
+    private Context context;
+    private ArrayList<User> usersList;
+    private int resource;
+
+    public ProfileAdapter(@NonNull Context context, int resource, ArrayList<User> usersList) {
+        super(context, resource, usersList);
+        this.resource = resource;
+        this.context = context;
+        this.usersList = usersList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater
+                    .from(this.context)
+                    .inflate(this.resource, parent, false);
+        }
+
+        TextView usernameField = view.findViewById(R.id.username_text_view);
+
+        User user = usersList.get(position);
+
+        usernameField.setText(user.getUsername());
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/vybe/SearchProfilesActivity.java
+++ app/src/main/java/com/example/vybe/SearchProfilesActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SearchProfilesActivity extends AppCompatActivity {
 
     private static final String TAG = "SearchProfilesActivity";
 
+    private ArrayList<User> usersList;
+    private ArrayList<User> searchList;
+    private ProfileAdapter profileAdapter;
+
     private SearchView searchView;
+    private ListView searchListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +30,48 @@ public class SearchProfilesActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: In search profiles");
 
         searchView = findViewById(R.id.search_view);
+        searchListView = findViewById(R.id.search_list_view);
+
+        usersList = new ArrayList<>();
+        usersList.add(new User("trishia", ""));
+        usersList.add(new User("haha", ""));
+        usersList.add(new User("hehe", ""));
+
+        searchList = new ArrayList<>();
+        profileAdapter = new ProfileAdapter(this, R.layout.user_item, searchList);
+        searchListView.setAdapter(profileAdapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                profileAdapter.clear();
+
+                for (User user: usersList) {
+                    String username = user.getUsername().toLowerCase();
+                    if (username.contains(query.toLowerCase())) {
+                        profileAdapter.add(user);
+                    }
+                }
+
+                if (profileAdapter.isEmpty()) {
+                    Toast.makeText(SearchProfilesActivity.this, "No match found", Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                User user = profileAdapter.getItem(position);
+//                System.out.println(user.getUsername());
+            }
+        });
     }
 }
