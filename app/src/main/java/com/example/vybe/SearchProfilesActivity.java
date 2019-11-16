7109e77f3e85e67080bea96b652f997c0cdf3773
Trishia Gatilogo <gatilogo@ualberta.ca>
2019-11-15 23:07:40 -0700

    created search view

--- app/src/main/java/com/example/vybe/SearchProfilesActivity.java
+++ app/src/main/java/com/example/vybe/SearchProfilesActivity.java
@@ -3,12 +3,21 @@ package com.example.vybe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SearchView;
 
 public class SearchProfilesActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchProfilesActivity";
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_profiles);
+        Log.d(TAG, "onCreate: In search profiles");
+
+        searchView = findViewById(R.id.search_view);
     }
 }
