748f53aaee1ac7c73901b89b4cda587ea2e400cd
Pranav Bodawala <b.pranav.k@gmail.com>
2019-11-17 03:38:27 -0700

    Social Situation Enums Integrated into Social Situation Field Fragment

--- app/src/main/java/com/example/vybe/AddEdit/SocialSituationFieldFragment.java
+++ app/src/main/java/com/example/vybe/AddEdit/SocialSituationFieldFragment.java
@@ -8,19 +8,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.vybe.Models.SocialSituation;
 import com.example.vybe.R;
+
+import java.util.List;
 
 public class SocialSituationFieldFragment extends Fragment {
 
-    private Spinner socStnDropdown;
     private OnSocStnSelectedListener onSocStnSelectedListener;
     private Context context;
+    private Spinner socStnDropdown;
+    private ImageButton clearBtn;
 
     interface OnSocStnSelectedListener {
         void onSocStnSelected(String socStn);
@@ -40,20 +46,49 @@ public class SocialSituationFieldFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_social_situation_field, container, false);
 
         socStnDropdown = view.findViewById(R.id.soc_stn_dropdown);
+        clearBtn = view.findViewById(R.id.clear_btn);
 
-        String[] socStn = new String[]{"Select a Social Situation", "Alone", "In a group", "Alone in a group"};
-        ArrayAdapter<String> socialSituationAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, socStn);
-        socStnDropdown.setAdapter(socialSituationAdapter);
+        ArrayAdapter<String> socStnAdapter = createSocStnAdapter();
+        socStnDropdown.setAdapter(socStnAdapter);
+
+        int hintPosition = socStnAdapter.getCount();
+        socStnDropdown.setSelection(hintPosition);
+
         socStnDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                onSocStnSelectedListener.onSocStnSelected(socStn[position]);
+
+                if (position != hintPosition) {
+                    onSocStnSelectedListener.onSocStnSelected(SocialSituation.at(position).toString());
+
+                } else {
+                    onSocStnSelectedListener.onSocStnSelected(null);
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) { }
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+        clearBtn.setOnClickListener((View v) -> {
+            socStnDropdown.setSelection(hintPosition);
         });
 
         return view;
+    }
+
+
+    private ArrayAdapter<String> createSocStnAdapter() {
+        List<String> options = SocialSituation.stringValues();
+        options.add("Select a Social Situation");
+
+        return new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, options) {
+            @Override
+            public int getCount() {
+                // don't display last item. It is used as hint.
+                int count = super.getCount();
+                return count > 0 ? count - 1 : count;
+            }
+        };
     }
 }
