1690b710c2c691df8e10772a0dad14a532e50388
Jacob Wong <jdw2@ualberta.ca>
2019-11-18 14:48:22 -0700

    Removed Comments

--- app/src/main/java/com/example/vybe/MyVibesActivity.java
+++ app/src/main/java/com/example/vybe/MyVibesActivity.java
@@ -145,16 +145,7 @@ public class MyVibesActivity extends AppCompatActivity {
                                             vibeEvent.setLatitude(doc.getDouble("latitude"));
                                             vibeEvent.setLongitude(doc.getDouble("longitude"));
                                         }
-
-//                                            LocalDateTime ldt = doc.getDate("datetime").toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-//                                            Log.d(TAG, ldt.toString());
-//                                            String reason = (String) doc.getData().get("reason");
-//                                            String socSit = (String) doc.getData().get("socSit");
-//                                            String id = (String) doc.getData().get("ID");
-//                                            String vibe = (String) doc.getData().get("vibe");
-//                                            String image = (String) doc.getData().get("image");
-//                                            double latitude = (double) doc.getData().get("latitude");
-//                                            double longitude = (double) doc.getData().get("longitude");
+                                        
                                         if (allFlag) {
                                             vibeEventList.add(vibeEvent);
                                         } else {
