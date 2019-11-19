4cec36d06fd32e3dce623cc6225eaee1c1bb011e
Trishia Gatilogo <gatilogo@ualberta.ca>
2019-11-19 12:05:32 -0700

    changed adapter to display the vibe name instead of the reason

--- app/src/main/java/com/example/vybe/MyVibesAdapter.java
+++ app/src/main/java/com/example/vybe/MyVibesAdapter.java
@@ -30,7 +30,7 @@ public class MyVibesAdapter extends ArrayAdapter<VibeEvent> {
     private ArrayList<VibeEvent> vibeEventList;
     private int resource;
     private TextView dateField;
-    private TextView reasonField;
+    private TextView vibeNameField;
     private ImageView vibeImage;
 
     public MyVibesAdapter(@NonNull Context context, int resource, ArrayList<VibeEvent> vibeEventList) {
@@ -51,7 +51,7 @@ public class MyVibesAdapter extends ArrayAdapter<VibeEvent> {
         }
 
         dateField = view.findViewById(R.id.view_date_text_view);
-        reasonField = view.findViewById(R.id.my_reason_text_view);
+        vibeNameField = view.findViewById(R.id.vibe_name_text_view);
         vibeImage = view.findViewById(R.id.image_view);
 
         VibeEvent vibeEvent = vibeEventList.get(position);
@@ -76,14 +76,6 @@ public class MyVibesAdapter extends ArrayAdapter<VibeEvent> {
         String dateFieldText = month + dateTime.format(formatter);
         dateField.setText(dateFieldText);
         vibeImage.setImageResource(vibeEvent.getVibe().getEmoticon());
-
-        // TODO: DO WE WANT TO DISPLAY REASON?
-        // KEN SAID ON FORUM DATETIME AND EMOTICON IS ENOUGH.
-        if (vibeEvent.getReason() == null) {
-            reasonField.setVisibility(View.GONE);
-        } else {
-            reasonField.setText(vibeEvent.getReason());
-        }
-
+        vibeNameField.setText(vibeEvent.getVibe().getName());
     }
 }
