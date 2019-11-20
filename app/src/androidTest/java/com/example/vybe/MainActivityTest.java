143afcfd76bc37bb6da895a84cd9104970d377db
thebromoe <kebbi@ualberta.ca>
2019-11-20 10:29:06 -0700

    start espresso tests

--- app/src/androidTest/java/com/example/vybe/MainActivityTest.java
+++ app/src/androidTest/java/com/example/vybe/MainActivityTest.java
@@ -1,8 +1,16 @@
 package com.example.vybe;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Test class for MainActivity. All the UI tests are written here. The Robotium test framework is
@@ -10,5 +18,13 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest{
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+        new ActivityTestRule<>(MainActivity.class, true, true);
+
+    @Test
+    public void listGoesOverTheFold(){
+        onView(withText("Vybe")).check(matches(isDisplayed()));
+    }
 
 }
