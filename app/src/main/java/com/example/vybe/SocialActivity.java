6265fdc3ce9e40570b89e81e174ea070f8b791d9
raysarinas <raysarinas97@gmail.com>
2019-11-22 00:47:01 -0700

    start on/make base for social vibes

--- app/src/main/java/com/example/vybe/SocialActivity.java
+++ app/src/main/java/com/example/vybe/SocialActivity.java
@@ -1,12 +1,26 @@
 package com.example.vybe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.vybe.Models.VibeEvent;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 /**
  * SocialActivity displays the screen for a user to view a list of their followers' vibe events
@@ -15,8 +29,16 @@ public class SocialActivity extends AppCompatActivity {
 
     private static final String TAG = "SocialActivity";
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    private ArrayList<VibeEvent> vibeEventList;
+    private MyVibesAdapter socialVibesAdapter;
+//    private String socialVibeEventDBPath;
     private Button myVibesBtn;
     private Button searchBtn;
+    private RecyclerView socialVibesRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +48,9 @@ public class SocialActivity extends AppCompatActivity {
 
         myVibesBtn = findViewById(R.id.my_vibes_btn);
         searchBtn = findViewById(R.id.search_btn);
+        socialVibesRecyclerView = findViewById(R.id.social_vibe_list);
+
+        buildRecyclerView();
 
         myVibesBtn.setOnClickListener((View v) -> {
             finish();
@@ -34,5 +59,23 @@ public class SocialActivity extends AppCompatActivity {
         searchBtn.setOnClickListener((View v) -> {
             startActivity(new Intent(SocialActivity.this, SearchProfilesActivity.class));
         });
+    }
+
+    //TODO: Add onStart() method to populate social vibes list
+
+    private void buildRecyclerView() {
+        vibeEventList = new ArrayList<>();
+
+        // TODO: remove this temp. hardcoded in list
+        VibeEvent socialtest = new VibeEvent();
+        socialtest.setVibe("surprised");
+        vibeEventList.add(socialtest);
+
+        socialVibesAdapter = new MyVibesAdapter(this, R.layout.my_vibe_item, vibeEventList);
+        socialVibesRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        socialVibesRecyclerView.setAdapter(socialVibesAdapter);
+
+        DividerItemDecoration itemDecor = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        socialVibesRecyclerView.addItemDecoration(itemDecor);
     }
 }
