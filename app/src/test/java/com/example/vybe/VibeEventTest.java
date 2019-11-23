7e9a57bc9a16e1e332764b859edea050b80247d6
Mohammad Kebbi <kebbi@ualberta.ca>
2019-11-23 01:06:42 -0700

    update VibeEventTest

--- app/src/test/java/com/example/vybe/VibeEventTest.java
+++ app/src/test/java/com/example/vybe/VibeEventTest.java
@@ -1,5 +1,7 @@
 package com.example.vybe;
 
+import android.icu.text.SimpleDateFormat;
+
 import com.example.vybe.Models.VibeEvent;
 import com.example.vybe.Models.vibefactory.Vibe;
 import com.example.vybe.Models.vibefactory.VibeFactory;
@@ -16,13 +18,13 @@ import static org.junit.Assert.assertNull;
 
 public class VibeEventTest {
 
-    private Date date = new Date();
-
     private VibeEvent mockEmptyVibeEvent() {
         return new VibeEvent();
     }
 
     private VibeEvent mockVibeEvent() {
+        Date date = new Date();
+
         return new VibeEvent(mockVibe().getName(), date,
                 "just really happy", "Alone", "", "image", 2.0, 2.0);
     }
@@ -32,35 +34,55 @@ public class VibeEventTest {
     }
 
     @Test
-    public void testEmptyConstructor() {
-        // TODO: talk about how to test
-        assertNull(mockEmptyVibeEvent().getVibe());
-        assertNull(mockEmptyVibeEvent().getReason());
-        assertNull(mockEmptyVibeEvent().getSocialSituation());
-        assertNull(mockEmptyVibeEvent().getId());
-        assertNull(mockEmptyVibeEvent().getImage());
+    public void VibeEventConstructor_EmptyInit() {
+        VibeEvent testVibeEvent = mockEmptyVibeEvent();
+
+        assertNull(testVibeEvent.getVibe());
+        assertNull(testVibeEvent.getReason());
+        assertNull(testVibeEvent.getSocialSituation());
+        assertNull(testVibeEvent.getId());
+        assertNull(testVibeEvent.getImage());
     }
 
     @Test
-    public void testNonEmptyConstructor() {
-        assertEquals(mockVibe().getName(), mockVibeEvent().getVibe().getName());
-//        assertEquals(ldt, mockVibeEvent().getDateTime());
-        assertEquals("just really happy", mockVibeEvent().getReason());
-        assertEquals("Alone", mockVibeEvent().getSocialSituation());
-        assertEquals("", mockVibeEvent().getId());
-        assertEquals("image", mockVibeEvent().getImage());
+    public void VibeEventConstructor_FullInit() {
+        VibeEvent testVibeEvent = mockVibeEvent();
+        Vibe testVibe = mockVibe();
+
+        Date testDate = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("MMMM dd, yyyy hh:mm a");
+
+        assertEquals(testVibe.getName(), testVibeEvent.getVibe().getName());
+        assertEquals("just really happy", testVibeEvent.getReason());
+        assertEquals(testDate.getClass(), testVibeEvent.getDateTime().getClass());
+        assertEquals("Alone", testVibeEvent.getSocialSituation());
+        assertEquals("", testVibeEvent.getId());
+        assertEquals("image", testVibeEvent.getImage());
     }
 
     @Test
-    public void testGetVibe() {
-        assertEquals(mockVibe().getName(), mockVibeEvent().getVibe().getName());
-        assertEquals(mockVibe().getEmoticon(), mockVibeEvent().getVibe().getEmoticon());
-        assertEquals(mockVibe().getColor(), mockVibeEvent().getVibe().getColor());
+    public void GetVibe() {
+        VibeEvent testVibeEvent = mockVibeEvent();
+        Vibe testVibe = mockVibe();
+
+        assertEquals(testVibe.getName(), testVibeEvent.getVibe().getName());
+        assertEquals(testVibe.getEmoticon(), testVibeEvent.getVibe().getEmoticon());
+        assertEquals(testVibe.getColor(), testVibeEvent.getVibe().getColor());
     }
 
-    @Ignore
-    public void GetDateTimeString() {
+    @Test
+    public void SetVibe(){
+        VibeEvent testVibeEvent = mockEmptyVibeEvent();
+
+        assertNull(testVibeEvent.getVibe());
+
+        Vibe testVibe = mockVibe();
+
+        assertEquals(testVibe.getName(), testVibeEvent.getVibe().getName());
+        assertEquals(testVibe.getEmoticon(), testVibeEvent.getVibe().getEmoticon());
+        assertEquals(testVibe.getColor(), testVibeEvent.getVibe().getColor());
 
     }
+
 
 }
