0f84ae07ae113a0291a67506ba3bdf48692031d0
Pranav Bodawala <b.pranav.k@gmail.com>
2019-11-25 12:02:06 -0700

    Move getId() for new vibe functionality to addEditController

--- app/src/main/java/com/example/vybe/AddEdit/AddEditController.java
+++ app/src/main/java/com/example/vybe/AddEdit/AddEditController.java
@@ -8,6 +8,8 @@ import com.example.vybe.Models.SocSit;
 import com.example.vybe.Models.Vibe;
 import com.example.vybe.Models.VibeEvent;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -15,7 +17,12 @@ import java.util.HashMap;
 
 public class AddEditController {
     private static AddEditController instance;
+
     private AddEditVibeEventActivity activity;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private String vibeEventDBPath = "Users/" + mAuth.getCurrentUser().getUid() + "/VibeEvents";
+    private VibeEvent vibeEvent;
 
     private AddEditController() {
     }
@@ -33,12 +40,21 @@ public class AddEditController {
     }
 
     public void editVibeEvent(VibeEvent vibeEvent) {
+        this.vibeEvent = vibeEvent;
         Vibe vibe = vibeEvent.getVibe();
         String reason = vibeEvent.getReason();
         SocSit socSit = vibeEvent.getSocSit();
+        assert vibeEvent.getId() != null : "Editing a VibeEvent that doesn't have an ID";
 
         activity.setVibe(vibe);
         if (reason != null) activity.setReason(reason);
         if (socSit != null) activity.setSocSit(socSit);
+    }
+
+    public void addVibeEvent() {
+        vibeEvent = new VibeEvent();
+
+        String id = db.collection(vibeEventDBPath).document().getId();
+        vibeEvent.setId(id);
     }
 }
