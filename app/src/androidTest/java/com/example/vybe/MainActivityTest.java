f8c3788c1e0688de473b8dfa82a249fd6874c369
Mohammad Kebbi <kebbi@ualberta.ca>
2019-11-25 20:53:55 -0700

    Logging in tests works and update documentation

--- app/src/androidTest/java/com/example/vybe/MainActivityTest.java
+++ app/src/androidTest/java/com/example/vybe/MainActivityTest.java
@@ -6,6 +6,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,65 +17,98 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
- * Test class for MainActivity. All the UI tests are written here. The Robotium test framework is
- used
+ * This tests for user login and authentication on the main login  activity
+ * This also tests for registration and creating a user account and using it to login
+ *
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     private String invalidLoginEmailNoExist = "non-existent@user.ca";
-    private String invalidLoginPassword = "invalid123";
+    private String invalidLoginPassword = "invaliduser";
     private String invalidLengthPassword = "1234";
 
 
     private String validLoginEmail = "espresso@test.ca";
     private String validLoginPassword = "vibecheck";
 
+    MainActivity activity;
+
     @Rule
     public ActivityTestRule<MainActivity> activityRule =
             new ActivityTestRule<>(MainActivity.class);
 
+    @Before
+    public void init(){
+        activity = activityRule.getActivity();
+    }
+
     @Test
-    public void InvalidLogin_NonExistantUser(){
+    public void InvalidLogin_EmptyParameters() throws InterruptedException {
+        onView(withId(R.id.login_button)).perform(click());
+
+        Thread.sleep(1000);
+
+        // Check we are still in login page
+        onView(withId(R.id.signup_button))
+                .check(matches(withText("Sign Up")));
+
+    }
+
+    @Test
+    public void InvalidLogin_NonExistantUser() throws InterruptedException {
         onView(withId(R.id.email_edit_text))
                 .perform(typeText(invalidLoginEmailNoExist), closeSoftKeyboard());
         onView(withId(R.id.password_edit_text))
                 .perform(typeText(invalidLoginPassword), closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
 
+        Thread.sleep(1000);
+
+        /* TODO: try to confirm toast displays message using
+          https://stackoverflow.com/questions/28390574/checking-toast-message-in-android-espresso
+          If time allows */
+
         // Check we are still in login page
         onView(withId(R.id.signup_button))
                 .check(matches(withText("Sign Up")));
     }
 
     @Test
-    public void InvalidLogin_TooShortPassword() {
+    public void InvalidLogin_TooShortPassword() throws InterruptedException {
         onView(withId(R.id.email_edit_text))
                 .perform(typeText(invalidLoginEmailNoExist), closeSoftKeyboard());
         onView(withId(R.id.password_edit_text))
                 .perform(typeText(invalidLengthPassword), closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
 
+        Thread.sleep(1000);
+
         // Check we are still in login page
         onView(withId(R.id.signup_button))
                 .check(matches(withText("Sign Up")));
     }
 
     @Test
-    public void validLogin() {
+    public void validLogin() throws InterruptedException {
         onView(withId(R.id.email_edit_text))
                 .perform(typeText(validLoginEmail), closeSoftKeyboard());
         onView(withId(R.id.password_edit_text))
                 .perform(typeText(validLoginPassword), closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
 
+        Thread.sleep(2000);
+
         // Check we logged in and we are on myVibes page
         onView(withId(R.id.filter_spinner))
-                .check(matches(withText("Filter Vibe")));
-
+                .check(matches(isDisplayed()));
     }
 
 
