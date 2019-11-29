3b279a3e93bb5bc62a35d9228a30a33eef070514
Mohammad Kebbi <kebbi@ualberta.ca>
2019-11-28 23:08:23 -0700

    add test suites

---
+++ app/src/androidTest/java/com/example/vybe/TestSuite.java
@@ -0,0 +1,18 @@
+package com.example.vybe;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        CreateAccountActivity.class,
+        LoginActivityTest.class,
+        ViewProfileActivity.class,
+        MyVibesActivity.class,
+        SocialActivityTest.class,
+        MapFragmentTest.class
+})
+
+public class TestSuite {
+}
---
+++ app/src/test/java/com/example/vybe/TestSuite.java
@@ -0,0 +1,15 @@
+package com.example.vybe;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        VibeTest.class,
+        UserTest.class,
+        VibeEventTest.class
+})
+
+public class TestSuite {
+}
